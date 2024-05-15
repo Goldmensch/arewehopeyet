2321f5eabc8932f9bb87343351f6f81433642dd3
Nick Hensel <47005420+Goldmensch@users.noreply.github.com>
2024-05-15 17:35:12 +0200

    Update index.js

--- www/src/index.js
+++ www/src/index.js
@@ -4,10 +4,12 @@ const REPO = "goldmensch/hope";
 const START_TIME = 1674738628000.0;
 
 function getPhrase(days) {
-  if (days == 0) {
-    return "A good day to be AP";
-  } else {
-    return "Get off Discord";
+  switch (days) {
+    case 0: return "The revolution is picking up speed.";
+    case 3: return "Is Jack more important again? Little reminder: You're not fucking Rose!"; 
+    case 7: return "Get on the revolution again! Marx would be absolutly disappointed in you!"; 
+    case 14: return "It's been 14 days now.. seems like the revolution failed, but its time will come again!";
+    default: return "Stop watching fucking netflix!";
   }
 }
 
